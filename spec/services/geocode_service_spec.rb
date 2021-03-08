e54a208e4e7a1776d6d39dfbd9ba3cd54b310e13
Catherine Dean <catherinemdean15@gmail.com>
2021-03-08 10:07:03 -0700

    add test for new geocode service method search_directions

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -9,4 +9,18 @@ describe GeocodeService do
     expect(data[:results].first[:locations]).to be_an(Array)
     expect(data[:results].first[:locations].first[:displayLatLng]).to be_a(Hash)
   end
+
+  it 'returns directions', :vcr do
+    start = 'GrandRapids,MI'
+    destination = 'Holland,MI'
+    data = GeocodeService.search_directions(start, destination)
+
+    expect(data).to have_key(:route)
+    expect(data[:route]).to have_key(:legs)
+    expect(data[:route]).to have_key(:formattedTime)
+    expect(data[:route][:formattedTime]).to eq('00:29:32')
+    expect(data[:route][:legs].first).to have_key(:maneuvers)
+    expect(data[:route][:legs].first[:maneuvers]).to be_an(Array)
+    expect(data[:route][:legs].first[:maneuvers].count).to eq(8)
+  end
 end
