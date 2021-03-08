730e16c817e5a00b06f59e9e7712b9a6e1894cae
Catherine Dean <catherinemdean15@gmail.com>
2021-03-08 10:49:42 -0700

    update forecast facade to have get_current_temperature method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,5 +4,12 @@ class ForecastFacade
       data = ForecastService.search_forecast(geocode)
       Forecast.new(data)
     end
+
+    def get_current_temperature(geocode)
+      # this utilizes the same service call but creates a different poro
+      # this is just for time-saving and potential caching purposes, but could reasonably be a different call that returns less infomration
+      data = ForecastService.search_forecast(geocode)
+      CurrentTemperature.new(data)
+    end
   end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -13,4 +13,12 @@ describe ForecastFacade do
     expect(data.daily_weather).to be_an(Array)
     expect(data.daily_weather.first).to be_a(DailyWeather)
   end
+
+  it 'returns a current temperature poro', :vcr do
+    geocode = Geocode.new({ lat: 39.738453, lng: -104.984853 })
+    data = ForecastFacade.get_current_temperature(geocode)
+    expect(data).to be_a(CurrentTemperature)
+    expect(data.temperature).to eq(51.49)
+    expect(data.summary).to eq('overcast clouds')
+  end
 end
