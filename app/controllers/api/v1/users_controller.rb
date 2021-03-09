6e361b9bb6b8437045277e00ea35bab370498112
Catherine Dean <catherinemdean15@gmail.com>
2021-03-09 11:46:41 -0700

    create users controller with create action

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if matching_password? && user.save
+      user.update({api_key: ApiKey.generator})
+      render json: UserSerializer.new(user), status: 201
+    else
+      (render json: {error: 'passwords do not match'}, status: 401 unless matching_password?) ||
+      (render json: {error: 'unable to register new user'}, status: 400)
+    end
+  end
+
+
+  private
+
+  def user_params
+    params[:email].downcase!
+    params.permit(:email, :password)
+  end
+
+  def matching_password?
+    params[:password] == params[:password_confirmation]
+  end
+end
