704ec6c84c98c6a154f154268fa2dcedadd1f898
Catherine Dean <catherinemdean15@gmail.com>
2021-03-09 19:19:24 -0700

    update for road trip functionality

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    @location = params[:location]
-    @geocode = GeocodeFacade.get_geocode(@location)
-    @forecast_data = ForecastFacade.get_forecast(@geocode)
-    render json: ForecastSerializer.new(@forecast_data)
+    if !params[:location].empty? && params[:location]
+      @location = params[:location]
+      @geocode = GeocodeFacade.get_geocode(@location)
+      @forecast_data = ForecastFacade.get_forecast(@geocode)
+      render json: ForecastSerializer.new(@forecast_data)
+    else
+      render json: {error: 'location must be included'}, status: 404
+    end
   end
 end
--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -7,4 +7,22 @@ class Forecast
     @hourly_weather = data[:hourly].first(8).map { |hour| HourlyWeather.new(hour) }
     @daily_weather = data[:daily].first(5).map { |day| DailyWeather.new(day) }
   end
+
+  def find_weather(travel_time)
+    if travel_time != "impossible"
+      arrival_time = arrival_hour(travel_time)
+      arrival_temp = hourly_weather.find do |hour|
+        hour.time.split(":")[0] == arrival_time
+      end
+      {temperature: arrival_temp.temperature, conditions: arrival_temp.conditions}
+    end
+  end
+
+  def arrival_hour(travel_time)
+    split_time = travel_time.split(":")
+    departure_time = Time.now
+    departure_time += (split_time[0].to_i * 60 * 60)
+    departure_time += (split_time[1].to_i * 60)
+    departure_time.strftime('%I')
+  end
 end
